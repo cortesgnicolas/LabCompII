1f01ba93cdd027e845581883645fe307ecaeb65e
cortesgnicolas <nicolascortes2704@gmail.com>
2023-08-11 12:00:08 -0300

    Entrega del primer ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main (String args[]){
         System.out.println("Hola Mundo");
+
+        System.out.println("Ingrese 3 valores, uno por uno: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int n3 = scanner.nextInt();
+
+        if (n1>n2 && n2>n3){
+            System.out.println(""+n1);
+            System.out.println(""+n2);
+            System.out.println(""+n3);
+        }
+        else if (n1>n2 && n3>n2){
+            System.out.println(""+n1);
+            System.out.println(""+n3);
+            System.out.println(""+n2);
+        }
+
+        else if (n2>n1 && n1>n3){
+            System.out.println(""+n2);
+            System.out.println(""+n1);
+            System.out.println(""+n3);
+        }
+        else if (n2>n3 && n3>n1){
+            System.out.println(""+n2);
+            System.out.println(""+n3);
+            System.out.println(""+n1);
+        }
+        else if (n3>n1 && n2>n1){
+            System.out.println(""+n3);
+            System.out.println(""+n2);
+            System.out.println(""+n1);
+        }
+        else if (n3>n1 && n1>n2){
+            System.out.println(""+n3);
+            System.out.println(""+n1);
+            System.out.println(""+n2);
+        }
     }
 
 }
